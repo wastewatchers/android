226a4b5865048f4e4c4d507a67ce016d2f8eb32e
Unknown <dajg1995@gmail.com>
2018-05-27 00:02:59 +0200

    made details hidable

--- app/src/main/java/io/github/wastewatchers/AddActivity.java
+++ app/src/main/java/io/github/wastewatchers/AddActivity.java
@@ -3,12 +3,14 @@ package io.github.wastewatchers;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -17,6 +19,8 @@ public class AddActivity extends AppCompatActivity {
 
     protected ImageButton mTakePictureButton;
     protected LinearLayout mPictureLayout;
+    protected TextView mDetailTextView;
+    protected ConstraintLayout mDetailsContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,7 @@ public class AddActivity extends AppCompatActivity {
         dpScale = getResources().getDisplayMetrics().density;
 
         mPictureLayout = findViewById(R.id.pictureLayout);
+        mDetailsContainer = findViewById(R.id.detailsContainer);
 
         mTakePictureButton = findViewById(R.id.takePictureButton);
         mTakePictureButton.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +43,23 @@ public class AddActivity extends AppCompatActivity {
 
             }
         });
+
+
+        mDetailTextView = findViewById(R.id.detailsTextView);
+        mDetailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = mDetailTextView.getText().toString();
+                name = name.substring(1, name.length());
+                if(mDetailsContainer.getVisibility() == View.VISIBLE){
+                    mDetailsContainer.setVisibility(View.GONE);
+                    mDetailTextView.setText("▲" + name);
+                }else{
+                    mDetailsContainer.setVisibility(View.VISIBLE);
+                    mDetailTextView.setText("▼" + name);
+                }
+            }
+        });
     }
 
     @Override
@@ -46,6 +68,7 @@ public class AddActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
+
 
             ImageView newImageView = new ImageView(this);
             newImageView.setImageBitmap(imageBitmap);
