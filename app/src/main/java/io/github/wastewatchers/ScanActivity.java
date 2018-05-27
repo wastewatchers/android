875597ceb2e0f06a4893b7be90103db3b4f3106b
Lukas Wagner <lukas.wagner@student.hpi.de>
2018-05-27 10:17:53 +0200

    fetch rating count

--- app/src/main/java/io/github/wastewatchers/ScanActivity.java
+++ app/src/main/java/io/github/wastewatchers/ScanActivity.java
@@ -1,15 +1,24 @@
 package io.github.wastewatchers;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class ScanActivity extends AppCompatActivity {
+    private static int sMinRatings = 5;
+    private static String sServerIP = "10.42.0.1:8080";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +37,38 @@ public class ScanActivity extends AppCompatActivity {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-                startActivity(new Intent(this, AddActivity.class));
+
+                RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+                String url = "http://" + sServerIP + "/rating/" + result.getContents() + "/count";
+                Log.d("scan", "url: " + url);
+
+                final Context context = this;
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("scan", "Response is: " + response);
+
+                            int count = Integer.parseInt(response);
+
+                            if(count < sMinRatings)
+                            {
+                                startActivity(new Intent(context, AddActivity.class));
+                            }
+                            else
+                            {
+                                // start product view activity
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("scan", "That didn't work: " + error);
+                    }
+                });
+
+                queue.add(stringRequest);
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
